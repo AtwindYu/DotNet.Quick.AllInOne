9b74a7847ebb140832dc6d04d6cba79e25e99179
YU-PC\Yu <atwind@cszi.com>
2018-01-08 18:14:49 +0800

    Parallel end

--- sources/xUnit2.Demo.Tests/Parallel.Demo/ParallelDemoTest.cs
+++ sources/xUnit2.Demo.Tests/Parallel.Demo/ParallelDemoTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -133,6 +135,149 @@ namespace xUnit2.Demo.Tests.Parallel.Demo
 
          
          */
+
+
+        /*
+         ** Parallel.ForEach也能对IEnumerable<int>集合进行重构。Enumerable.Range生产了序列化的数目。但这样就没有上面的分区效果。
+         */
+
+        [Fact]
+        public void ParallelForEachGenerateMD5HasHes()
+        {
+            var sw = Stopwatch.StartNew();
+            System.Threading.Tasks.Parallel.ForEach(Enumerable.Range(1, NUM_AES_KEYS), number =>
+            {
+                var md5M = MD5.Create();
+                byte[] data = Encoding.Unicode.GetBytes(Environment.UserName + number);
+                byte[] result = md5M.ComputeHash(data);
+                string hexString = ConverToHexString(result);
+                _output.WriteLine($"Number:{number}");
+            });
+            _output.WriteLine("MD5:" + sw.Elapsed.ToString() + ":");
+        }
+
+
+
+
+        /*从循环中退出
+         
+        和串行运行中的break不同，ParallelLoopState 提供了两个方法用于停止Parallel.For 和 Parallel.ForEach的执行。
+
+        Break:让循环在执行了当前迭代后尽快停止执行。比如执行到100了，那么循环会处理掉所有小于100的迭代。
+        Stop:让循环尽快停止执行。如果执行到了100的迭代，那不能保证处理完所有小于100的迭代。
+
+         */
+        [Fact]
+        private void ParallelForEachGenerateMD5HasHesBreak()
+        {
+            var sw = Stopwatch.StartNew();
+            var loopresult = System.Threading.Tasks.Parallel.ForEach(Enumerable.Range(1, NUM_AES_KEYS * 10), (int number, ParallelLoopState loopState) =>
+              {
+                  var md5M = MD5.Create();
+                  byte[] data = Encoding.Unicode.GetBytes(Environment.UserName + number);
+                  byte[] result = md5M.ComputeHash(data);
+                  string hexString = ConverToHexString(result);
+                  if (sw.Elapsed.Seconds > 1)
+                  {
+                      loopState.Stop();
+                  }
+              });
+            ParallelLoopResult(loopresult);
+            _output.WriteLine("MD5:" + sw.Elapsed);
+        }
+
+        private void ParallelLoopResult(ParallelLoopResult loopResult)
+        {
+            string text;
+            if (loopResult.IsCompleted)
+            {
+                text = "循环完成";
+            }
+            else
+            {
+                if (loopResult.LowestBreakIteration.HasValue)
+                {
+                    text = "Break终止";
+                }
+                else
+                {
+                    text = "Stop 终止";
+                }
+            }
+            _output.WriteLine(text);
+        }
+
+
+
+        /*
+         捕捉并行循环中发生的异常。
+          当并行迭代中调用的委托抛出异常，这个异常没有在委托中被捕获到时，就会变成一组异常，新的System.AggregateException负责处理这一组异常。
+         */
+        [Fact]
+        public void ParallelForEachGenerateMD5HasHesException()
+        {
+            var sw = Stopwatch.StartNew();
+            var loopresult = new ParallelLoopResult();
+            try
+            {
+                loopresult = System.Threading.Tasks.Parallel.ForEach(Enumerable.Range(1, NUM_AES_KEYS * 10), (number, loopState) =>
+                  {
+                      var md5M = MD5.Create();
+                      byte[] data = Encoding.Unicode.GetBytes(Environment.UserName + number);
+                      byte[] result = md5M.ComputeHash(data);
+                      string hexString = ConverToHexString(result);
+                      if (sw.Elapsed.Seconds > 1)
+                      {
+                          throw new TimeoutException("执行超过1秒");
+                      }
+                  });
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var innerEx in ex.InnerExceptions)
+                {
+                    _output.WriteLine(innerEx.ToString());
+                }
+            }
+
+            ParallelLoopResult(loopresult);
+            _output.WriteLine("MD5:" + sw.Elapsed);
+        }
+
+
+
+
+        /*指定并行度。
+         
+         TPL的方法总会试图利用所有可用的逻辑内核来实现最好的结果，但有时候你并不希望在并行循环中使用所有的内核。
+         比如你需要留出一个不参与并行计算的内核，来创建能够响应用户的应用程序，而且这个内核需要帮助你运行代码中的其他部分。这个时候一种好的解决方法就是指定最大并行度。
+
+        这需要创建一个ParallelOptions的实例，设置MaxDegreeOfParallelism的值。
+         */
+
+        [Theory]
+        [InlineData(2)]
+        public void ParallelMaxDegree(int maxDegree)
+        {
+            var parallelOptions = new ParallelOptions();
+            parallelOptions.MaxDegreeOfParallelism = maxDegree;
+
+            var sw = Stopwatch.StartNew();
+            System.Threading.Tasks.Parallel.For(1, NUM_AES_KEYS + 1, parallelOptions, (int i) =>
+            {
+                var aesM = new AesManaged();
+                aesM.GenerateKey();
+                byte[] result = aesM.Key;
+                string hexStr = ConverToHexString(result);
+            });
+            _output.WriteLine("AES:" + sw.Elapsed.ToString());
+        }
+
+        [Fact]
+        public void ParallelMaxDegreeRun()
+        {
+            ParallelMaxDegree(Environment.ProcessorCount - 1);
+        }
 
     }
 }
