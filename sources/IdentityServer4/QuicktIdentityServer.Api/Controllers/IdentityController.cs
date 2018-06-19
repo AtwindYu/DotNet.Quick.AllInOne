8a4f2600f8fe3352132b4be5a69fa3ab30362950
Office\Yu <atwind@cszi.com>
2018-06-19 12:01:16 +0800

    API完成，API里已经不用额外添加什么引用包了。MS已经包含在App中了。可以直接书写IdentityController。注意看Start类中的修改。 http://localhost:56784/Identity

---
+++ sources/IdentityServer4/QuicktIdentityServer.Api/Controllers/IdentityController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuicktIdentityServer.Api.Controllers
+{
+
+    [Route("[controller]")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+
+        // GET
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(User.Claims.Select(x => new { x.Type, x.Value }));
+        }
+    }
+}
