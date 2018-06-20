2a3f2d1e57eedf0cf9f3e59c2817bde488a47f6a
Office\Yu <atwind@cszi.com>
2018-06-20 15:39:12 +0800

    OpenIdConnect 下的退出功能完成。

--- sources/IdentityServer4/QuickIdentityServer.MvcClient/Controllers/HomeController.cs
+++ sources/IdentityServer4/QuickIdentityServer.MvcClient/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using QuickIdentityServer.MvcClient.Models;
@@ -24,6 +25,25 @@ namespace QuickIdentityServer.MvcClient.Controllers
             ViewData["Message"] = "Your application description page.";
 
             return View();
+        }
+
+
+        /// <summary>
+        /// 退出授权登录
+        /// <remarks>
+        /// 最后一步是给MVC客户端添加注销功能。
+        /// 使用IdentityServer等身份验证服务，仅清除本地应用程序Cookie是不够的。 此外，您还需要往身份服务器交互，以清除单点登录会话。
+        /// 确切的协议步骤在OpenID Connect中间件内实现，只需将以下代码添加到某个控制器即可触发注销.
+        /// 
+        /// 
+        /// 这将清除本地cookie，然后重定向到IdentityServer。 IdentityServer将清除它的cookie，然后给用户一个链接返回到MVC应用程序。
+        /// </remarks>
+        /// </summary>
+        /// <returns></returns>
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
         }
 
         public IActionResult Contact()
