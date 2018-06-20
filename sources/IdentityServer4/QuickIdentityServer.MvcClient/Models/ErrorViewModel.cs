79ff4ce4d0481b26b1c6bc03be788cc0ad704bff
Office\Yu <atwind@cszi.com>
2018-06-20 11:30:59 +0800

    创建一个MVC客户端

---
+++ sources/IdentityServer4/QuickIdentityServer.MvcClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuickIdentityServer.MvcClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
